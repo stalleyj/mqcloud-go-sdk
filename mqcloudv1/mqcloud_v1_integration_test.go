4baa568cf334d4d149f00dc07e74b5a1ffe3f652
SOWMYA SRIDHAR <SowmyaGrama@in.ibm.com>
2023-12-18 13:02:05 +0530

    fix:replace println with Fprintf
    Signed-off-by: SOWMYA SRIDHAR <SowmyaGrama@in.ibm.com>

--- mqcloudv1/mqcloud_v1_integration_test.go
+++ mqcloudv1/mqcloud_v1_integration_test.go
@@ -546,7 +546,7 @@ var _ = Describe(`MqcloudV1 Integration Tests`, func() {
 			filePath := truststore_filepath // Replace with your file path
 			file, err := os.Open(filePath)
 			if err != nil {
-				fmt.Println(GinkgoWriter, fmt.Errorf("Error opening file: %s", err))
+				fmt.Fprintf(GinkgoWriter, fmt.Errorf("Error opening file: %s", err))
 				return
 			}
 			defer file.Close()
@@ -644,7 +644,7 @@ var _ = Describe(`MqcloudV1 Integration Tests`, func() {
 			filePath := keystore_filepath // Replace with your file path
 			file, err := os.Open(filePath)
 			if err != nil {
-				fmt.Println(GinkgoWriter, fmt.Errorf("Error opening file: %s", err))
+				fmt.Fprintf(GinkgoWriter, fmt.Errorf("Error opening file: %s", err))
 				return
 			}
 			defer file.Close()
